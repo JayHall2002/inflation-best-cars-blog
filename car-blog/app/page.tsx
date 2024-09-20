3e4ff914296e66833b02f27620d4e19e729d72d2
JayHall2002 <Ahallaj20002@gmail.com>
2024-09-20 12:41:26 -0500

    Added title Aj's Car Blog and used tailwind / globalcss to stlye

--- car-blog/app/page.tsx
+++ car-blog/app/page.tsx
@@ -1,14 +1,20 @@
 import Head from "next/head";
+import './global.css';
+
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Car Blog</title>
+        <title>Aj's Car Blog</title>
         <meta name="description" content="Find the best cars in every segment at your price point." />
       </Head>
+      <h1 className="titleonhome 
+      text-6xl font-bold text-center mt-4 
+      webkit-font-smoothing: antialiased 
+      font-style: italic text-gray-400 hover:text-gray-600">
+        Aj's Car Blog
+      </h1>
 
-      <h1>Hello, Welcome to the Car Blog!</h1>
-      <p>This Blog will help you find the best car in every segment</p>
     </div>
   );
 }
