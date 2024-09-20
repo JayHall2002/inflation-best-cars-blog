507c441353d21ab3a1b22cca260cb211cf485a4e
JayHall2002 <Ahallaj20002@gmail.com>
2024-09-20 14:35:58 -0500

    Add comments to code for greater organization and added group hover effects

--- car-blog/app/page.tsx
+++ car-blog/app/page.tsx
@@ -22,23 +22,47 @@ export default function Home() {
       text-center mt-6  font-mono text-blue-400 tracking-tight">
         Learn About the Best Cars in Each Segment
       </h2>
+
+      
+      {/*Performance Car Segment Logo and Effects*/}
       <div className="flex flex-row justify-center mt-9 items-end">
-        <div className="text-center mx-4">
+        <div className="text-center mx-4 relative group">
           <Image src={performancelogo} alt="Performance Car Segment" width={300} height={300}
-          className="performance-logo mt-9 h-64 scale-75 blur-custom-sm hover:blur-none
-          transition ease-in-out delay-150 hover:scale-90" />
+          className="performance-logo mt-9 h-64 scale-75 blur-custom-sm group-hover:blur-none
+          transition ease-in-out delay-150 group-hover:scale-90" />
+          <div
+          className="absolute inset-0 flex items-center justify-center text-white 
+          font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Click to Explore Segment 
+          </div>
           <p className="font-mono text-blue-400 tracking-tight">Performance Cars</p>
         </div>
-        <div className="text-center mx-4">
+
+
+        {/*Economy Car Segment Logo and Effects*/}
+        <div className="text-center mx-4 relative group">
           <Image src={economylogo} alt="Economy Car Segment" width={300} height={300} 
-          className="mt-9 h-64 scale-75 blur-custom-sm hover:blur-none
-          transition ease-in-out delay-150 hover:scale-90"/>
+          className="mt-9 h-64 scale-75 blur-custom-sm group-hover:blur-none
+          transition ease-in-out delay-150 group-hover:scale-90"/>
+          <div
+          className="absolute inset-0 flex items-center justify-center text-white 
+          font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Click to Explore Segment 
+          </div>
           <p className="font-mono text-blue-400 tracking-tight">Economy Cars</p>
         </div>
-        <div className="text-center mx-4">
+
+
+        {/*Electric Car Logo and Effects*/}
+        <div className="text-center mx-4 relative group">
           <Image src={electriclogo} alt="Electric Car Segment" width={300} height={300} 
-          className="mt-9 h-64 scale-75 blur-custom-sm hover:blur-none 
-          transition ease-in-out delay-150 hover:scale-90"/>
+          className="mt-9 h-64 scale-75 blur-custom-sm group-hover:blur-none 
+          transition ease-in-out delay-150 group-hover:scale-90"/>
+          <div
+          className="absolute inset-0 flex items-center justify-center text-white 
+          font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Click to Explore Segment 
+          </div>
           <p className="font-mono text-blue-400 tracking-tight">Electric Cars</p>
         </div>
       </div>
