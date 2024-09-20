735db88b5c6aac6e812bee19781d7ee15e02013a
JayHall2002 <Ahallaj20002@gmail.com>
2024-09-20 14:21:11 -0500

    Added three logos for the three main car segments with effects in preparation for routing

--- car-blog/app/page.tsx
+++ car-blog/app/page.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
+import Image from "next/image";
 import './global.css';
+import performancelogo from './images/performancecarlogo.jpeg';
+import economylogo from './images/economygaslogo.jpg';
+import electriclogo from './images/electriccarlogo.jpeg';
 
 export default function Home() {
   return (
@@ -14,7 +18,31 @@ export default function Home() {
       font-style: italic text-gray-400 hover:text-gray-600">
         Aj's Car Blog
       </h1>
-
+      <h2 className="descriptiononhome 
+      text-center mt-6  font-mono text-blue-400 tracking-tight">
+        Learn About the Best Cars in Each Segment
+      </h2>
+      <div className="flex flex-row justify-center mt-9 items-end">
+        <div className="text-center mx-4">
+          <Image src={performancelogo} alt="Performance Car Segment" width={300} height={300}
+          className="performance-logo mt-9 h-64 scale-75 blur-custom-sm hover:blur-none
+          transition ease-in-out delay-150 hover:scale-90" />
+          <p className="font-mono text-blue-400 tracking-tight">Performance Cars</p>
+        </div>
+        <div className="text-center mx-4">
+          <Image src={economylogo} alt="Economy Car Segment" width={300} height={300} 
+          className="mt-9 h-64 scale-75 blur-custom-sm hover:blur-none
+          transition ease-in-out delay-150 hover:scale-90"/>
+          <p className="font-mono text-blue-400 tracking-tight">Economy Cars</p>
+        </div>
+        <div className="text-center mx-4">
+          <Image src={electriclogo} alt="Electric Car Segment" width={300} height={300} 
+          className="mt-9 h-64 scale-75 blur-custom-sm hover:blur-none 
+          transition ease-in-out delay-150 hover:scale-90"/>
+          <p className="font-mono text-blue-400 tracking-tight">Electric Cars</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
