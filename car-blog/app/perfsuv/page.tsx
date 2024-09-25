9d2844c4d1b73398c63bd89787b38386f7ca4b99
JayHall2002 <Ahallaj20002@gmail.com>
2024-09-25 11:12:49 -0500

    Added title and navbar to Perf SUV Page.

--- car-blog/app/perfsuv/page.tsx
+++ car-blog/app/perfsuv/page.tsx
@@ -1,6 +1,28 @@
 {/* When Performance Suv is selected it will take you here */}
-export default function Perfsedans () {
+
+{/* Necessary Imports */}
+import Navbar from "../components/Navbar";
+
+export default function Perfsuvs () {
     return (
-        <h1>Performance Sedans</h1>
+        <div>
+            <div className="relative">
+                < Navbar/> {/* Navbar at the top of the screen */}
+                {/* Import the perfsuv Intro video underneath of the Navbar */}
+                
+                <a href="https://www.tiktok.com/@zzvne.edits/video/7062267449236000006?q=x6m%20rocars&t=1727279751790" target="_blank" rel="noopener noreferrer">
+                    <video autoPlay muted loop
+                    className="w-full rounded-lg blur-custom-sm shadow-lg"
+                    style={{height:'500px', objectFit:'cover', objectPosition:'center'}}>
+                        <source src="/videos/perfsuvintro.mp4" type="video/mp4" />
+                    </video>
+                    {/* Performance SUV Title */}
+                    <div className="absolute inset-0 flex items-center 
+                    justify-center text-white text-4xl font-mono font-bold">
+                        <h1>Performance SUVS</h1>
+                    </div>
+                </a>
+            </div>
+        </div>
     );
 }
